df8fa2effbec39bc45b5e73d41394a18e2d321be
nvia62 <wrg7412@gmail>
2021-03-30 10:03:04 +0900

    first commit

---
+++ dev/food.py
@@ -0,0 +1,12 @@
+for i in range(1, 4):
+  print("밥묵으러 가자")
+  print("치킨 피자 햄버거")
+  a = str(input(str(i) + " 선택:"))
+  print("%s를 선택하셨습니다" % a)
+  if(a=='치킨') :
+      print("맛잇다")
+  elif(a=='피자') :
+      print("맛있다!!")
+  elif(a=='햄버거') :
+      print("맛앗다")
+      print("잘못 선택하셧습니다")
