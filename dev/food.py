40d31e8458ff5700e859a3f4804b7cde1da826d2
nvia62 <wrg7412@gmail>
2021-03-30 12:47:06 +0900

    랜덤 추가 그리고 리스트

--- dev/food.py
+++ dev/food.py
@@ -4,9 +4,10 @@ for i in range(1, 4):
   a = str(input(str(i) + " 선택:"))
   print("%s를 선택하셨습니다" % a)
   if(a=='치킨') :
-      print("맛잇다")
+    print("맛잇다")
   elif(a=='피자') :
-      print("맛있다!!")
+    print("맛있다!!")
   elif(a=='햄버거') :
-      print("맛앗다")
-      print("잘못 선택하셧습니다")
+    print("맛앗다")
+  else :
+    print("잘못 선택하셧습니다")
